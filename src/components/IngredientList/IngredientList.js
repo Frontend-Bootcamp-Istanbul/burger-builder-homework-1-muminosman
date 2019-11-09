56328610492188d5a3d518e87ff1fb9eeb27b40c
muminosman <muminoth@gmail.com>
2019-11-09 16:23:53 +0300

    mumin osman

---
+++ src/components/IngredientList/IngredientList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function IngredientList()
+{
+return(
+<div>
+IngredientList
+
+</div>)
+
+}
+
+export default IngredientList;
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 /**
  * Created by mehmetyurtar on 11/9/19.
  */
-import Hamburger from "./Hamburger";
-import TotalPrice from "./TotalPrice";
+import Hamburger from "./Hamburger"
+import TotalPrice from "./TotalPrice"
+import IngredientList from "./IngredientList"
 
-export {Hamburger, TotalPrice};
+
+export  {Hamburger,TotalPrice,IngredientList};
